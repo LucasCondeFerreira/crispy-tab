4ea4d3f3e4a2093d5e0220ebb456e8574c69ba66
Lucas Conde Ferreira <81776746+LucasCondeFerreira@users.noreply.github.com>
2025-07-16 00:49:01 +0000

    Commit de teste

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+return <h1>Eu to te enviando esse link, porque você é minha parceira pra tudo, passeios, lanches, filmes, caminhadas, conversas, trapalhadas, acertos, momentos tristes e momentos felizes, eu amo viver minha vida fazendo as coisas com você, inclusive em estudos e eu to testando uma tecnologia nova, me da uma resposta quando você ler isso. <br /> Eu te amo ❤️ </ h1>
+}
+
+export default Home;
